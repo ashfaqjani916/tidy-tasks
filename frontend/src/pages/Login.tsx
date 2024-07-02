6691b116dfd49f67d2cef0718fa397de68214dbe
ashfaqjani916 <ashfaqjani916@gmail.com>
2024-07-02 19:06:48 +0530

    [fix] sensitive data removed

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,6 @@
 import { Button } from '../components/ui/button'
 import { useState, useEffect } from 'react'
+import 'dotenv/config'
 
 const Login = () => {
   const [error, setError] = useState<string | null>(null)
@@ -17,12 +18,12 @@ const Login = () => {
   }, [])
   const handleGoogleLogin = () => {
     const params = new URLSearchParams({
-      client_id: '396362726266-ko91smlcg079d2edpopq88l9i5c0os0j.apps.googleusercontent.com',
-      redirect_uri: 'http://localhost:8000/api/auth/callback/google',
+      client_id: '',
+      redirect_uri: '',
       response_type: 'code',
       scope: 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/userinfo.email ',
       state: new URLSearchParams({
-        redirect_uri: 'http://localhost:8000/api/auth/callback/google',
+        redirect_uri: '',
       }).toString(),
     })
     window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?${params}`
